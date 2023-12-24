91bddc9ed35a3a84f55b1376b9ff26c1bbba8f98
M.can <caglayan.mertcan37@gmail.com>
2023-12-25 01:19:40 +0300

    mousemove updated

--- script.js
+++ script.js
@@ -53,7 +53,9 @@ window.onmousemove = (event) => {
 
 	imageGalleryDiv.dataset.percentage = nextPercentage;
 
-	imageGalleryDiv.style.transform = `translate(${nextPercentage}%)`;
+    imageGalleryDiv.animate({
+        transform:`translate(${nextPercentage}%)`
+    }, {duration: 1100, fill:"forwards"});
 
 };
 
@@ -74,11 +76,39 @@ window.ontouchmove = (event) => {
 	const touchDelta = parseFloat(imageGalleryDiv.dataset.touchStartX) - event.touches[0].clientX;
 	const maxDelta = window.innerWidth / 2;
 
-	const percentage = (touchDelta / maxDelta) * -100;
-	console.log(percentage);
+	let percentage = (touchDelta / maxDelta) * -100;
+    let nextPercentage = parseFloat(imageGalleryDiv.dataset.prevPercentage) + percentage;
+
+	nextPercentage = Math.min(nextPercentage, 100);
+	nextPercentage = Math.max(nextPercentage, -100);
+
+	imageGalleryDiv.dataset.percentage = nextPercentage;
 
 
     imageGalleryDiv.animate({
-        transform:`translate(${percentage}%)`
+        transform:`translate(${nextPercentage}%)`
     }, {duration: 1100, fill:"forwards"});
+};
+
+
+window.onwheel = (event) => {
+    event.preventDefault();
+
+    const wheelDelta = event.deltaY;
+    const maxDelta = window.innerHeight / 2;
+
+    // Adjust the factor to control the scrolling speed
+    const scrollFactor = 0.21; // Adjust this value to control the scroll speed (21% in this case)
+
+    let percentage = (wheelDelta / maxDelta) * scrollFactor;
+    let nextPercentage = parseFloat(imageGalleryDiv.dataset.prevPercentage) + percentage;
+
+    nextPercentage = Math.min(nextPercentage, 21);
+    nextPercentage = Math.max(nextPercentage, -21);
+
+    imageGalleryDiv.dataset.percentage = nextPercentage;
+
+    imageGalleryDiv.animate({
+        transform: `translate(${nextPercentage}%)`
+    }, { duration: 1100, fill: "forwards" });
 };
